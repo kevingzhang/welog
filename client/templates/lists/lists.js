c1657086dc50c1659389152232bc6e9505779289
farcryzry <farcryzry@gmail.com>
2015-02-07 13:20:26 -0800

    posts data source

--- client/templates/lists/lists.js
+++ client/templates/lists/lists.js
@@ -1,9 +1,5 @@
 Template.lists.helpers({
-  times: function () {
-    var times = [];
-    _(20).times(function(n){
-      times.push(n);
-    });
-    return times;
+  posts: function() {
+    return Posts.find();
   }
 });
