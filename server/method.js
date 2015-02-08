2b5361ba171421573cd4b5c5af2f7ef076f2a5d7
Jason <xiaoyao890718@gmail.com>
2015-02-08 13:08:05 -0800

    store author id

--- server/method.js
+++ server/method.js
@@ -1,7 +1,6 @@
 Meteor.methods({
 	// you can call this in browser console: Meteor.call('newUser', 'test', 'test@gmail.com', 'test')
 	newUser: function(user, email, password){
-		console.log ('user',user,email,password);
 		Accounts.createUser({username: user, email: email, password: password});
 	}
 });
